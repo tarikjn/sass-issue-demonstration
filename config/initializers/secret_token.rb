ee73d887cb7f0d65a5d5b16c47932424558d917b
Tarik Ansari <tarikjn@gmail.com>
2011-09-01 23:02:14 -0700

    clean 3.1.0 rails project, after using rails new *

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testsass::Application.config.secret_token = 'cabbafcf6729da59948277b20224807fccf332b5a552342020f2bad8597394b0525944f06037dd522633fcfbad4c9818f8d3f2c77a497444e14577b368addea9'
